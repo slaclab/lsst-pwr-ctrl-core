d9a86d1f82bae29548b6e9bae67b0b905574cd76
Larry Ruckman <ruckman@slac.stanford.edu>
2021-01-16 12:24:09 -0800

    Updating submodule locks

--- ruckus.tcl
+++ ruckus.tcl
@@ -9,7 +9,7 @@ if { [VersionCheck 2017.4 ] < 0 } {
 # Check for submodule tagging
 if { [info exists ::env(OVERRIDE_SUBMODULE_LOCKS)] != 1 || $::env(OVERRIDE_SUBMODULE_LOCKS) == 0 } {
    if { [SubmoduleCheck {ruckus} {2.9.0}  "mustBeExact" ] < 0 } {exit -1}
-   if { [SubmoduleCheck {surf}   {2.12.2} "mustBeExact" ] < 0 } {exit -1}
+   if { [SubmoduleCheck {surf}   {2.12.3} "mustBeExact" ] < 0 } {exit -1}
 } else {
    puts "\n\n*********************************************************"
    puts "OVERRIDE_SUBMODULE_LOCKS != 0"
